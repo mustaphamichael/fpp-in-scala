9b59fb42dcdbbfbb9cca4a204c17ebcfac618380
Michael Mustapha <mustaphamichael75@gmail.com>
2020-07-11 18:00:11 +0100

    Week 2 - Functional Sets

---
+++ src/main/scala/week2/Main.scala
@@ -0,0 +1,7 @@
+package week2
+
+import FunSets._
+
+object Main extends App {
+  println(contains(singletonSet(1), 1))
+}
---
+++ src/test/scala/week2/FunSetSuite.scala
@@ -0,0 +1,125 @@
+package week2
+
+import org.junit._
+
+/**
+ * This class is a test suite for the methods in object FunSets.
+ *
+ * To run this test suite, start "sbt" then run the "test" command.
+ */
+class FunSetSuite {
+
+  import FunSets._
+
+  @Test def `contains is implemented`: Unit = {
+    assert(contains(x => true, 100))
+  }
+
+  /**
+   * When writing tests, one would often like to re-use certain values for multiple
+   * tests. For instance, we would like to create an Int-set and have multiple test
+   * about it.
+   *
+   * Instead of copy-pasting the code for creating the set into every test, we can
+   * store it in the test class using a val:
+   *
+   * val s1 = singletonSet(1)
+   *
+   * However, what happens if the method "singletonSet" has a bug and crashes? Then
+   * the test methods are not even executed, because creating an instance of the
+   * test class fails!
+   *
+   * Therefore, we put the shared values into a separate trait (traits are like
+   * abstract classes), and create an instance inside each test method.
+   *
+   */
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  /**
+   * This test is currently disabled (by using @Ignore) because the method
+   * "singletonSet" is not yet implemented and the test would fail.
+   *
+   * Once you finish your implementation of "singletonSet", remvoe the
+   *
+   * @Ignore annotation.
+   */
+  @Test def `singleton set one contains one`: Unit = {
+
+    /**
+     * We create a new instance of the "TestSets" trait, this gives us access
+     * to the values "s1" to "s3".
+     */
+    new TestSets {
+      /**
+       * The string argument of "assert" is a message that is printed in case
+       * the test fails. This helps identifying which assertion failed.
+       */
+      assert(contains(s1, 1), "Singleton")
+    }
+  }
+
+  @Test def `union contains all elements of each set`: Unit = {
+    new TestSets {
+      val s = union(s1, s2) // {1, 2}
+      assert(contains(s, 1), "Union 1")
+      assert(contains(s, 2), "Union 2")
+      assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  @Test def `intersect contains only unique elements of each set`: Unit = {
+    new TestSets {
+      val s = intersect(union(s1, s2), union(s1, s3)) // {1}
+      assert(contains(s, 1), "Intersect 1")
+      assert(!contains(s, 2), "Intersect 2")
+    }
+  }
+
+  @Test def `diff contains elements in set S not in set T`: Unit = {
+    new TestSets {
+      val s = diff(union(s1, s2), union(s1, s3)) // {2}
+      assert(contains(s, 2), "Diff 1")
+      assert(!contains(s, 1), "Diff 2")
+    }
+  }
+
+  @Test def `filter contains elements in a set for which a predicate holds`: Unit = {
+    new TestSets {
+      val s = union(union(s1, s2), union(s1, s3)) // {1,2,3}
+      val fs = filter(s, (x => x % 2 != 0)) // filter out even elements
+      assert(FunSets.toString(fs) == "{1,3}", "Filter 1")
+      assert(!contains(fs, 2), "Filter 2")
+    }
+  }
+
+  @Test def `check all elements in a set to satisfy a predicate`: Unit = {
+    new TestSets {
+      val s = forall(union(s1, s3), x => x % 2 != 0) // true
+      assert(s, "ForAll 1")
+    }
+  }
+
+  @Test def `check if an element in a set exists that satisfies a predicate`: Unit = {
+    new TestSets {
+      val s = exists(union(s1, s2), x => x % 2 == 0) // true
+      assert(s, "Exists 1")
+    }
+  }
+
+  @Ignore
+  @Test def `map elements in a set`: Unit = {
+    new TestSets {
+      val s = map(s1, x => x * 2) // {2, 4}
+      printSet(s)
+      assert(contains(s, 4), "Map 1")
+    }
+  }
+
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+}
